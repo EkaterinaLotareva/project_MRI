d7d986456a835bcc4d7633a11e3d19f5c5720dbc
Kate Lotareva <k.v.lotareva@gmail.com>
2025-11-19 21:32:13 +0300

    исправление ошибок

--- inductance.py
+++ inductance.py
@@ -15,13 +15,15 @@ def function(theta, R1, R2, b1, b2, alpha):
     rho = sqrt(x ** 2 + y ** 2)
     k = sqrt((4*R1*rho)/((R1+rho)**2 + z**2))
 
+    if alpha == 0 & b1 == b2:
+        return 0
     return ((2*mu*sqrt(R1))/(k*sqrt(rho)*4*pi)) * ((1-k**2/2)*special.ellipk(k) - special.ellipe(k)) * \
            ((R2*cos(alpha) + x2*sin(theta))/(sqrt((x2+R2*cos(alpha)*sin(theta))**2 + (R2*cos(theta))**2))) * R2
 
 
 
 def inductance(R1, R2, b1, b2, alpha):
-    return scipy.integrate.quad(function, 0, 2*pi, args = (R1, R2, b1, b2, alpha))
+    return scipy.integrate.quad(function, 0, 2*pi, args = (R1, R2, b1, b2, alpha))[0]
 
 def inductance_matrix(n, m, R, A, a):
 
