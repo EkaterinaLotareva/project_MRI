3f2d765a02992252f4a4bad63c1ff71e4f2f6394
lizzzk <79269580+elizaveta-zh1508@users.noreply.github.com>
2025-12-22 12:58:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,33 +4,59 @@ from currents import *
 from magnetic_field import *
 import math
 
-R = [3.5, 3.5, 3.5, 3.5, 3.5] #см
-Delta = [1, 0.1, 1, 0.1] #см
-m = 4
-n = 5
-A = 8 #см
-w = math.pi*2*63.8 #Мгц
-U = None
-C = None
-S = 1
-rho = 1.724*(10**3)
-resistance = []
-
-for i in range(n):
-    resistance.append(rho*2*math.pi*R[i]/S)
-resistance = np.array(resistance)
+# Параметры системы
+m = 2          # Количество стопок
+rad = [0.035]  # Радиусы колец в стопке
+deltas = []   # пока 1 кольцо в стопке
+N_seg = 512 # мелкость разбиения колец
+n = len(rad) # количство колец в стопке
+sigma = 5.96e7 
+r = np.full(shape=(m, n), fill_value=0.001)  # радиус сечения кольца
+C = np.full(shape=(m, n), fill_value=0.001)  # емкость
+omega = 63.8e6  # частота
+x_off = rad[0]/2  # радиус внутренней зоны
 
 
-r = [] #относительные размеры
+R = [] #относительные размеры
 delta = [] #относительные размеры
 for i in range(len(R)):
-    r.append(R[i]/A)
+    r.append(rad[i]/x_off)
 for i in range(len(Delta)):
-    delta.append(Delta[i]/A)
+    delta.append(deltas[i]/x_off)
 
-MRI = SymmetricSystem(m, r, delta, n, A)
 
-L = inductance_matrix(n, m, r, 1, delta)
-I = currents(impedance(resistance, C, L, w), U)
-obs_points = None
-b_s_l(obs_points, I, MRI.N, MRI.n, MRI.m, all_coordinates=MRI.points_on_rings_general())
+system = SymmetricSystem(m=m, R=rad, delta=deltas, N=N_seg, n=n, A=x_off)
+all_coordinates = system.points_on_rings_general()
+
+
+# Получение матрицы взаимных индуктивностей
+L = inductance_matrix(n, m, rad, r, x_off, deltas)
+# U = np.array([3.+0.j, 3.+0.j, 3.+0.j, 3.+0.j])
+U = np.array(generate_U(2, m, n))
+demo_Z = demo_Z_self_matrix(sigma, r, C, n, m, rad)
+
+# Нахождение вектора токов
+I = calc_I(demo_Z, U, omega, L)
+I_matrix = I.reshape(m, n)
+
+
+x_max = x_off + sum(deltas) + rad[-1]
+x_min = -x_max
+x_coords = np.linspace(x_min, x_max, 200)
+y_coords = np.linspace(x_min, x_max, 200)
+
+# Точки наблюдения по оси x P_obs = (x, 0, 0)
+P_obs_X = np.stack([
+    x_coords,
+    np.zeros_like(x_coords),
+    np.zeros_like(x_coords)
+], axis=1)
+
+
+Bmagx = b_s_l(obs_points=P_obs_X,
+                 I_matrix=I_matrix,
+                 N=N_seg,
+                 n=n,
+                 m=m,
+                 all_coordinates=all_coordinates)
+
